0f5d97c28404c179f818a5466acce1436a8f9db5
Gabriel <gabriel.luthun@gmail.com>
2024-07-08 15:55:40 +0200

    feat(Program.cs): add input verifications and comments

--- Program.cs
+++ Program.cs
@@ -10,27 +10,24 @@
             Console.Write("Entrez un nombre (ou appuyez sur Entrée pour terminer) : ");
             string input = Console.ReadLine()!;
             if (input == "") break;
-
-            
-            if (!double.TryParse(input, out _)) 
+            if (input == "d")
             {
-                Console.WriteLine("Veuillez entrer un nombre valide.");
+                nombres.RemoveAt(nombres.Count - 1);
                 continue;
             }
-
             nombres.Add(Convert.ToDouble(input));
         }
     }
 
     static void Menu()
     {
-        //Interface graphique
+        // Interface graphique
         Console.WriteLine("Connexion en cours...");
-        Task.Delay(1000).Wait();
+        Task.Delay(750).Wait();
         Console.Clear();
 
-        //Menu de la calculatrice
-        Console.WriteLine("Calculatrice en C#");
+        // Menu de la calculatrice
+        Console.WriteLine("--------- Calculatrice en C# (tapez 8 pour de l'aide) ------------------");
         Console.WriteLine("1. Addition");
         Console.WriteLine("2. Soustraction");
         Console.WriteLine("3. Multiplication");
@@ -38,15 +35,36 @@
         Console.WriteLine("5. Puissances");
         Console.WriteLine("6. Racines");
         Console.WriteLine("7. Quitter");
+        Console.WriteLine("8. Commandes");
         Console.Write("Choisissez une option : ");
     }
 
-    static void Main()
+    static int verifyInput()
     {
+        int choix;
+        bool isInt;
         while (true)
         {
+            // Vérification de l'entrée utilisateur
+            isInt = int.TryParse(Console.ReadLine(), out choix);
+
+            // Si l'entrée n'est pas un nombre ou si le nombre n'est pas entre 1 et 7, on affiche un message d'erreur, puis on retourne au menu
+            if (isInt && choix >= 1 && choix <= 8) break;
+            Console.WriteLine("Veuillez entrer un nombre entre 1 et 8.");
+            Task.Delay(1000).Wait();
+            Console.Clear();
             Menu();
-            int choix = Convert.ToInt32(Console.ReadLine()!);
+        }
+        return choix;
+    }
+
+
+    static void Main()
+    {
+        while (true) // Tant que l'utilisateur ne quitte pas, on continue
+        {
+            Menu();
+            int choix = verifyInput();
             double result;
 
             switch (choix)
@@ -90,9 +108,17 @@
                     Console.WriteLine("Le résultat de la racine est : " + result);
                     break;
                 case 7:
+                    Console.Clear();
                     Console.WriteLine("Déconnecté avec succès !");
                     Task.Delay(500).Wait();
                     Environment.Exit(0);
+                    break;
+                case 8:
+                    Console.Clear();
+                    Console.WriteLine("Commandes :");
+                    Console.WriteLine("-----------------------");
+                    Console.WriteLine("Lors d'une opération, en cas d'erreur, utilisez la commande 'd' pour supprimer le dernier nombre entré");
+                    Console.WriteLine("Entrée : valider un nombre entré");
                     break;
                 default:
                     Console.WriteLine("Choix invalide");
